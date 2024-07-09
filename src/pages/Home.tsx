0923f63c30e94ca4e2fa6e263fd34907bcbfb90c
PuffCab <berlinkollner@gmail.com>
2024-07-09 11:26:10 +0200

    more unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { CharactersContext } from "../context/CharactersContext";
 import { AuthContext } from "../context/AuthContext";
 // import useFetchData from "../hooks/useFetchData";
